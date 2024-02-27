dfd5ae674c3d3ef4c8bb2f65fdcad3ab1e87a8f5
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-02-27 16:50:15 +0200

    Add files via upload

---
+++ Labs/01.JS-Front-End-JS-Syntax-Fundamentals-Lab/01.JS-Front-End-JS-Syntax-Fundamentals-Lab/month-printer.js
@@ -0,0 +1,33 @@
+function month(int) {
+    let num = ""
+    
+    if (int === 1) {
+        console.log('January');
+    }else if (int === 2) {
+        console.log('February');
+    }else if(int === 3) {
+        console.log('March');
+    }else if (int === 4) {
+        console.log('April');
+    }else if (int === 5) {
+        console.log('May');
+    }else if (int === 6) {
+        console.log('June');
+    }else if (int === 7) {
+        console.log('July');
+    }else if (int === 8) {
+        console.log('August')
+    }else if (int === 9) {
+        console.log('September')
+    }else if (int === 10) {
+        console.log('October')
+    }else if (int === 11) {
+        console.log('November')
+    }else if (int === 12) {
+        console.log('December')
+    }else {
+        console.log('Error!')
+    }
+}
+
+
