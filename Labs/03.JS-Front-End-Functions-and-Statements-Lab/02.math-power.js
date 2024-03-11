eca7944674feb8a9d0b37bf143babf4a1fb7b4e8
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-03-11 22:26:49 +0200

    Add files via upload

---
+++ Labs/03.JS-Front-End-Functions-and-Statements-Lab/02.math-power.js
@@ -0,0 +1,10 @@
+function mathPower(number, power) {
+    let result = 1;
+    for (let i = 0; i < power; i++) {
+        result *= number
+    }
+    console.log(`${result}`);
+
+}
+
+mathPower(2,8)
---
+++ Labs/03.JS-Front-End-Functions-and-Statements-Lab/03.repeat-string.js
@@ -0,0 +1,10 @@
+function repeatString (word, count) {
+    let result = ''
+    for (let i = 0; i < count; i++) {
+        result += word
+    }
+    console.log(`${result}`);
+}
+
+
+
