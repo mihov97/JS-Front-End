cb6e54b79a9efe87fa3412b412538e692412385a
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-03-14 23:22:27 +0200

    Delete Exercise/03.JS-Front-End-Functions-and-Statements-Exercise directory

--- Exercise/03.JS-Front-End-Functions-and-Statements-Exercise/02.add-subtract.js
+++
@@ -1,8 +0,0 @@
-function solve(num1, num2, num3) {
-    const sum = (a, b) => a + b;
-    const subtract = (a ,b) => a - b;
-
-    const result = subtract (sum(num1, num2), num3)
-
-    console.log(result)
-}
