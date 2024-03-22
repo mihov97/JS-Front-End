0cb41deb4fa4b734defb7537894f2115aa0dabd5
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-03-22 18:55:16 +0200

    Update 06.words.tracker.js

--- Exercise/04.JS-Front-end-Objects-and-Classes-Exercise/06.words.tracker.js
+++ Exercise/04.JS-Front-end-Objects-and-Classes-Exercise/06.words.tracker.js
@@ -1,17 +1,17 @@
-function findWords(wordsArr) {
+function words(input) {
     let searchWords = wordsArr[0].split(' ')
-    let wordsObj = {}
+    let obj = {}
 
-    for (let word1 of searchWords) {
-        wordsObj[word1] = 0
-        for (let word2 of wordsArr.slice(1)) {
-            if (word1 === word2) {
-                wordsObj[word1] += 1
+    for (let firstWord of searchWords) {
+        obj[firstWord] = 0
+        for (let secondWord of wordsArr.slice(1)) {
+            if (firstWord === secondWord) {
+                obj[firstWord] += 1
             }
         }
     }
 
-    for (let [key, value] of Object.entries(wordsObj).sort((a, b) => b[1] - a[1])) {
+    for (let [key, value] of Object.entries(obj).sort((a, b) => b[1] - a[1])) {
         console.log(`${key} - ${value}`)
     }
 }
