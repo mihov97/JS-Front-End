b0153f9a0d259b5fc1b52118f6f6070267065e55
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-04-15 22:36:24 +0300

    Update app.js

--- Labs/06.DOM-Manipulation-and-Events/01. List-Of-Items/app.js
+++ Labs/06.DOM-Manipulation-and-Events/01. List-Of-Items/app.js
@@ -1,11 +1,8 @@
 function addItem() {
     const inputEl = document.getElementById('newItemText');
     const itemEl = document.getElementById('items');
-
-    
     const newEl = document.createElement('li');
 
-    
     newEl.textContent = inputEl.value;
 
      itemEl.appendChild(newItemElement);
