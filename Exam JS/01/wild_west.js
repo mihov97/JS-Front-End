da840ddf03a054a6fb8198e978f82e321633790b
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-04-16 20:38:22 +0300

    Add files via upload

---
+++ Exam JS/01/wild_west.js
@@ -0,0 +1,87 @@
+function solve(input){
+    let characters = []
+ 
+    inputLines = input[0]
+    restOfAllCounter = 1
+ 
+    while(restOfAllCounter <= inputLines){
+        const [name, health, bullets] = input[restOfAllCounter].split(' ');
+        characters.push({name: name, health: Number(health), bullets: Number(bullets)});
+        restOfAllCounter += 1
+    }
+ 
+    function returningPerson(name){
+        return characters.find(heroName => heroName.name === name)
+    }
+ 
+    while(input[restOfAllCounter] !== 'Ride Off Into Sunset'){
+        
+        const [command, name, firstArg, secondArg] = input[restOfAllCounter].split(' - ')
+ 
+        if(command === 'FireShot'){
+            let person = returningPerson(name)
+            
+            if(person.bullets > 0){
+                console.log(`${name} has successfully hit ${firstArg} and now has ${person.bullets - 1} bullets!`);
+                person.bullets -= 1
+            
+            } else {
+                console.log(`${name} doesn't have enough bullets to shoot at ${firstArg}!`);
+            }
+        
+        } else if (command === 'TakeHit'){
+            
+            let person = returningPerson(name)
+            person.health -= Number(firstArg)
+            
+            let personName = `${person.name}`
+            
+            if(person.health > 0){
+                console.log(`${name} took a hit for ${firstArg} HP from ${secondArg} and now has ${person.health} HP!`);
+           
+            } else {
+                console.log(`${name} was gunned down by ${secondArg}!`);
+                characters = characters.filter(person => person.name != personName);
+            }
+       
+        
+        } else if (command === 'Reload'){
+            let person = returningPerson(name)
+            
+            if (person.bullets < 6){
+                console.log(`${name} reloaded ${6 - person.bullets} bullets!`);
+            
+            person.bullets = 6
+            
+        } else if (person.bullets == 6){
+                console.log(`${name}'s pistol is fully loaded!`);
+            }
+       
+        } else if (command ==='PatchUp'){
+            let person = returningPerson(name)
+            
+            
+            if (person.health + Number(firstArg) > 100){
+                console.log(`${name} patched up and recovered ${100 - person.health} HP!`);
+                person.health = 100
+            
+            
+            } else if (person.health + Number(firstArg) <= 100) {
+                console.log(`${name} patched up and recovered ${firstArg} HP!`);
+                
+                person.health += Number(firstArg)
+           
+            
+            } else {
+                console.log(`${name} is in full health!`);
+            }
+        }
+        
+        restOfAllCounter += 1
+    }
+    
+    for(let charecter of characters){
+        console.log(`${charecter.name}\n HP: ${charecter.health}\n Bullets: ${charecter.bullets}`);
+    }
+} 
+
---
+++ Exam JS/02/app.js
@@ -0,0 +1,108 @@
+window.addEventListener("load", solve);
+
+function solve() {
+    const addButtonElement = document.getElementById('add-btn');
+    const nameInputElement = document.getElementById('name');
+    const phoneNumberInputElement = document.getElementById('phone');
+    const categoryInputElement = document.getElementById('category');
+    const checkListElement = document.getElementById('check-list');
+    const contactsElement = document.getElementById('contact-list');
+    
+
+    addButtonElement.addEventListener('click', () => {
+        const name = nameInputElement.value;
+        const phone = phoneNumberInputElement.value;
+        const category = categoryInputElement.value;
+
+        if (!name || !phone || !category) {
+            return;
+        
+        } 
+
+        const checkLiElement = createArticleElement(name, phone, category);
+        checkListElement.appendChild(checkLiElement)
+
+       // addButtonElement.setAttribute('disabled', 'disabled'); //moje bi ne
+
+        nameInputElement.value = '';
+        phoneNumberInputElement.value = '';
+        categoryInputElement.value = '';
+
+        const editButtonElement = checkLiElement.querySelector('.edit-btn');
+        const saveButtonElement = checkLiElement.querySelector('.save-btn');
+
+        editButtonElement.addEventListener('click', () => {
+            
+            nameInputElement.value = name;
+            phoneNumberInputElement.value = phone;
+            categoryInputElement.value = category;
+
+            checkLiElement.remove();
+
+           // addButtonElement.removeAttribute('disabled'); // ne e sig
+        })
+
+        saveButtonElement.addEventListener('click', () => {
+
+            const checkEditButtonsElement = checkLiElement.querySelector('.edit-btn');
+            checkEditButtonsElement.remove();
+
+            const checkSaveButtonsElement = checkLiElement.querySelector('.save-btn');
+            checkSaveButtonsElement.remove();
+
+            const deleteButtonElement = document.createElement('button');
+            deleteButtonElement.classList.add('del-btn');
+            
+
+            checkLiElement.appendChild(deleteButtonElement);
+            contactsElement.appendChild(checkLiElement);
+
+            deleteButtonElement.addEventListener('click', () => {
+                contactsElement.remove()
+        
+            });
+
+        })
+    })
+
+    
+    
+    function createArticleElement (name, phone, category) {
+        const pNameElement = document.createElement('p');
+        pNameElement.textContent = `name:${name}`;
+
+        const pPhoneElement = document.createElement('p');
+        pPhoneElement.textContent = `phone:${phone}`;
+
+        const pCategoryElement = document.createElement('p');
+        pCategoryElement.textContent = `category:${category}`;
+
+        const articleElement = document.createElement('article');
+        articleElement.appendChild(pNameElement);
+        articleElement.appendChild(pPhoneElement);
+        articleElement.appendChild(pCategoryElement);
+
+        const editButtonElement = document.createElement('button');
+        editButtonElement.classList.add('edit-btn');
+        
+
+        const saveButtonElement = document.createElement('button');
+        saveButtonElement.classList.add('save-btn');
+       
+
+        const buttonsDivElement = document.createElement('div');
+        buttonsDivElement.classList.add('buttons');
+        buttonsDivElement.appendChild(editButtonElement);
+        buttonsDivElement.appendChild(saveButtonElement);
+
+        const liElement = document.createElement('li');
+        liElement.appendChild(articleElement);
+        liElement.appendChild(buttonsDivElement);    
+           
+
+    return liElement;
+    }
+    
+  }
+
+  
