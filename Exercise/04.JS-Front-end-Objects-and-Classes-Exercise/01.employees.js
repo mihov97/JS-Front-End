4142adf3643cdfce237a38351a9423524f7ec3a6
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-04-28 21:43:34 +0300

    Update 01.employees.js

--- Exercise/04.JS-Front-end-Objects-and-Classes-Exercise/01.employees.js
+++ Exercise/04.JS-Front-end-Objects-and-Classes-Exercise/01.employees.js
@@ -4,8 +4,7 @@ function solve(employeeNames) {
     for (const name of employeeNames) {
         employee[name] = name.length
     }
-
-   for (const employee in employees) {
+    for (const employee in employees) {
     console.log(`Name: ${employee} -- Personal Number: ${employees[employee]}`);
    }
 }
