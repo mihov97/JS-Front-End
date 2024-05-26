bfcf0c3824becbafd8d27e6d4e800fb5ebf49ce5
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-05-26 14:18:44 +0300

    Update 07-odd-occurances.js

--- Exercise/04.JS-Front-end-Objects-and-Classes-Exercise/07-odd-occurances.js
+++ Exercise/04.JS-Front-end-Objects-and-Classes-Exercise/07-odd-occurances.js
@@ -3,13 +3,16 @@ function solve(input) {
     const wordOcc = {};
 
     for (const word of words) {
+        
         if (!wordOcc.hasOwnProperty(word.toLowerCase())) {
             wordOcc[word.toLowerCase()] = 0;
         }
+        
         wordOccs[word.toLowerCase()]++;
     }
 
     const result = [];
+  
     for (const word in wordOcc) {
         
         if (wordOcc[word] % 2 !== 0) {
