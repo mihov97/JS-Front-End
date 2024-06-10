fd8087acc42cad283fa5169b9b72a8ff1d41b53f
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2024-06-10 21:55:17 +0300

    Add files via upload

---
+++ Labs/06.JS-Front-End-DOM-Events/06.DOM-Manipulation-and-Events/01. List-Of-Items/app.js
@@ -0,0 +1,16 @@
+function addItem() {
+    const inputEl = document.getElementById('newItemText');
+    const itemEl = document.getElementById('items');
+
+    
+    const newEl = document.createElement('li');
+
+    
+    newEl.textContent = inputEl.value;
+
+    
+    itemEl.appendChild(newItemElement);
+
+    
+    inputEl.value = '';
+}
